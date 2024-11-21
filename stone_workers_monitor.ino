8a719a59c0b00a0b4206be96891660ccc0a632be
arghasen10 <arghasen10@gmail.com>
2024-11-21 14:50:28 +0530

    added clock

--- stone_workers_monitor.ino
+++ stone_workers_monitor.ino
@@ -5,7 +5,7 @@
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 #include <ArduinoJson.h> 
-int nodeID = 10; 
+int nodeID = 7; 
 char syncedTime[32] = "01-01-1970 00:00:00";
 
 char tempStr[64];
@@ -14,8 +14,13 @@ bool enabled = false;
 AsyncWebServer server(80);
 PollutantSensors pol=PollutantSensors(0x08,nodeID);
 unsigned long lastSyncTime = 0;
-const unsigned long syncInterval = 60000;
+const unsigned long syncInterval = 5000;
 const char* serverName = "http://10.5.20.240:6001/sensor"; 
+
+unsigned long lastClockUpdate = 0;
+unsigned long clockInterval = 1000;
+int seconds = 0, minutes = 0, hours = 0;
+int day = 1, month = 1, year = 1970;
 
 int post_request(String httpRequestData) {
     WiFiClient client;
@@ -36,7 +41,7 @@ int post_request(String httpRequestData) {
 void request_sync_time() {
     WiFiClient client;
     HTTPClient http;
-    String timeServer = "http://10.5.20.240:6001/time";  // Replace with your endpoint
+    String timeServer = "http://10.5.20.240:6001/time"; 
 
     http.begin(client, timeServer);
     int httpResponseCode = http.GET();
@@ -48,11 +53,12 @@ void request_sync_time() {
         StaticJsonDocument<128> jsonDoc; 
         DeserializationError error = deserializeJson(jsonDoc, serverResponse);
         if (!error) {
-            const char* time = jsonDoc["time"];  // Extract "time" field
-            strncpy(syncedTime, time, sizeof(syncedTime));  // Safely copy to syncedTime
+            const char* time = jsonDoc["time"]; 
+            strncpy(syncedTime, time, sizeof(syncedTime)); 
             Serial.print("Time synchronized from server: ");
             Serial.println(syncedTime);
             pol.setSyncedTime(syncedTime);
+            sscanf(syncedTime, "%d-%d-%d %d:%d:%d", &day, &month, &year, &hours, &minutes, &seconds);
         } else {
             Serial.print("JSON parse error: ");
             Serial.println(error.c_str());
@@ -72,11 +78,38 @@ void handle_sync(AsyncWebServerRequest *request) {
         timeParam.toCharArray(syncedTime, 32);  
         Serial.print("Time synchronized: ");
         Serial.println(syncedTime);
+        // sscanf(syncedTime, "%d-%d-%d %d:%d:%d", &day, &month, &year, &hours, &minutes, &seconds);
         request->send_P(200, "text/html", "Time Set");
     } else {
         request->send_P(400, "text/html", "Missing time parameter");
     }
 }
+
+void updateClock() {
+    if (millis() - lastClockUpdate >= clockInterval) {
+        lastClockUpdate = millis();
+        seconds++;
+        if (seconds >= 60) {
+            seconds = 0;
+            minutes++;
+        }
+        if (minutes >= 60) {
+            minutes = 0;
+            hours++;
+        }
+        if (hours >= 24) {
+            hours = 0;
+            day++;  
+        }
+
+        Serial.printf("Current Time: %02d-%02d-%d %02d:%02d:%02d\n", day, month, year, hours, minutes, seconds);
+        snprintf(syncedTime, sizeof(syncedTime), "%02d-%02d-%d %02d:%02d:%02d", 
+                 day, month, year, hours, minutes, seconds);
+
+        pol.setSyncedTime(syncedTime);
+    }
+}
+
 
 void handle_con(AsyncWebServerRequest *request) {
     enabled = true;
@@ -117,6 +150,7 @@ void setup() {
 void loop() {
     wls.Maintain();
     pol.Sample(Serial2,Serial);
+    updateClock();
     if (millis() - lastSyncTime > syncInterval) {
         lastSyncTime = millis();
         request_sync_time();
